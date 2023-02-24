9549f32ca6103dc60efdca327c8fdf57083a7fca
jaroid <imthanhhai217@gmail.com>
2023-02-24 20:34:37 +0700

    update UI HomeActivity

--- app/src/main/java/com/jaroid/android51_demofirebase/HomeActivity.java
+++ app/src/main/java/com/jaroid/android51_demofirebase/HomeActivity.java
@@ -1,19 +1,19 @@
 package com.jaroid.android51_demofirebase;
 
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -24,10 +24,12 @@ public class HomeActivity extends AppCompatActivity {
     EditText edtMessage;
     @BindView(R.id.btnSend)
     Button btnSend;
-    @BindView(R.id.tvMessage)
-    TextView tvMessage;
+    @BindView(R.id.rvMessageHistory)
+    RecyclerView rvMessageHistory;
 
     private FirebaseDatabase firebaseDatabase;
+    private FirebaseUser firebaseUser;
+    private DatabaseReference databaseReference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,33 +41,26 @@ public class HomeActivity extends AppCompatActivity {
 
     private void initFirebase() {
         firebaseDatabase = FirebaseDatabase.getInstance();
+        firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+        databaseReference = firebaseDatabase.getReference();
     }
 
     private void initView() {
         ButterKnife.bind(this);
 
+        Log.d("TAG", "initView: " + firebaseUser);
         btnSend.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                sendMessageToFirebase(firebaseDatabase.getReference("message"), edtMessage.getText().toString());
+                sendMessageToFirebase();
             }
         });
 
-        firebaseDatabase.getReference("message").addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot snapshot) {
-                String value = snapshot.getValue(String.class);
-                tvMessage.setText(value);
-            }
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError error) {
-
-            }
-        });
     }
 
-    private void sendMessageToFirebase(DatabaseReference message, String s) {
-        message.setValue(s);
+    private void sendMessageToFirebase() {
+        String message = edtMessage.getText().toString().trim();
+        if (!TextUtils.isEmpty(message)) {
+        }
     }
 }
